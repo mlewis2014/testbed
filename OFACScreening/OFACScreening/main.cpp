38e6d4015f3130d3627970d56b281a2655f467b0
mlewis2014 <llewis.mark@gmail.com>
2014-11-15 12:20:18 -0600

    Initial creation

---
+++ OFACScreening/OFACScreening/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OFACScreening
+//
+//  Created by Mark on 11/15/14.
+//  Copyright (c) 2014 Mark. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
